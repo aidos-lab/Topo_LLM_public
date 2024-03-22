19e8edb921f94e7cd2e3a9a8e3e4501cdd6e992e
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-03-22 17:25:54 +0100

    Added a setup script for setting the environment variable for the repository base base; edited the config file path based on this

---
+++ setup_environment.sh
@@ -0,0 +1,13 @@
+# export TOPO_LLM_REPOSITORY_BASE_PATH=$HOME/git-source/Topo_LLM
+
+echo "Setting up environment variables for Topo_LLM"
+
+# Setup for Ben
+#
+echo "export TOPO_LLM_REPOSITORY_BASE_PATH=$HOME/git-source/Topo_LLM" >> $HOME/.bashrc
+echo "export TOPO_LLM_REPOSITORY_BASE_PATH=$HOME/git-source/Topo_LLM" >> $HOME/.zshrc
+
+# Setup for Julius
+#
+# echo "export TOPO_LLM_REPOSITORY_BASE_PATH=$HOME/Documents/LLM-Analysis/Topo_LLM" >> $HOME/.bashrc
+# echo "export TOPO_LLM_REPOSITORY_BASE_PATH=$HOME/Documents/LLM-Analysis/Topo_LLM" >> $HOME/.zshrc
