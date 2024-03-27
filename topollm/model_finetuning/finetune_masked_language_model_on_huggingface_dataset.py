1ce49b355d19c2eff13d3c36a7d396f477e2a2ca
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-03-27 17:06:29 +0100

    Extracted the steps in the finetuning script into separate functions

--- topollm/model_finetuning/finetune_masked_language_model_on_huggingface_dataset.py
+++ topollm/model_finetuning/finetune_masked_language_model_on_huggingface_dataset.py
@@ -37,11 +37,14 @@ Script for fine-tuning language model on huggingface datasets.
 # System imports
 import logging
 import math
+import os
 
 # Third party imports
+import datasets
 import hydra
 import hydra.core.hydra_config
 import omegaconf
+import torch
 import transformers
 from tqdm import tqdm
 from transformers import AutoModelForMaskedLM, AutoTokenizer, PreTrainedModel
@@ -49,15 +52,17 @@ from transformers import AutoModelForMaskedLM, AutoTokenizer, PreTrainedModel
 
 # Local imports
 from topollm.config_classes.MainConfig import MainConfig
+from topollm.config_classes.FinetuningConfig import FinetuningConfig
 from topollm.config_classes.path_management.FinetuningPathManagerFactory import (
     get_finetuning_path_manager,
+)
+from topollm.config_classes.path_management.FinetuningPathManagerProtocol import (
+    FinetuningPathManager,
 )
 from topollm.data_handling.DatasetPreparerFactory import get_dataset_preparer
 from topollm.logging.initialize_configuration_and_log import initialize_configuration
 from topollm.logging.setup_exception_logging import setup_exception_logging
 from topollm.model_handling.get_torch_device import get_torch_device
-from topollm.model_handling.load_model import load_model
-from topollm.model_handling.load_tokenizer import load_tokenizer
 
 # END Imports
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -112,7 +117,7 @@ def run_finetuning(
 ) -> None:
     finetuning_config = main_config.finetuning
 
-    # # # #
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Use accelerator if available
     device = get_torch_device(
         preferred_torch_backend=main_config.preferred_torch_backend,
@@ -140,23 +145,92 @@ def run_finetuning(
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Load tokenizer and model
 
-    # ? TODO Do we need to add parameters to the tokenizer here for finetuning?
+    tokenizer = load_tokenizer(
+        finetuning_config=finetuning_config,
+        logger=logger,
+    )
 
-    logger.info(
-        f"Loading tokenizer "
-        f"{finetuning_config.pretrained_model_name_or_path = } ..."
+    base_model = load_base_model(
+        finetuning_config=finetuning_config,
+        device=device,
+        logger=logger,
     )
-    tokenizer = AutoTokenizer.from_pretrained(
-        pretrained_model_name_or_path=finetuning_config.pretrained_model_name_or_path,
-    )
-    logger.info(
-        f"Loading tokenizer "
-        f"{finetuning_config.pretrained_model_name_or_path = } DONE"
-    )
-    logger.info(f"tokenizer:\n{tokenizer}")
-    # Make sure not to accidentally modify the tokenizer pad token (tokenizer.pad_token) here.
-    # In particular, it is not custom to set the pad token to the eos token for masked language model training.
 
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # Prepare model input
+
+    train_dataset_mapped, eval_dataset_mapped = prepare_model_input(
+        train_dataset=train_dataset,
+        eval_dataset=eval_dataset,
+        tokenizer=tokenizer,
+        finetuning_config=finetuning_config,
+    )
+
+    data_collator = transformers.DataCollatorForLanguageModeling(
+        tokenizer=tokenizer,
+        mlm=True,
+        mlm_probability=finetuning_config.mlm_probability,
+    )
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # Output paths
+
+    finetuning_path_manager = get_finetuning_path_manager(
+        config=main_config,
+        logger=logger,
+    )
+
+    finetuned_model_dir = prepare_finetuned_model_dir(
+        finetuning_path_manager=finetuning_path_manager,
+        logger=logger,
+    )
+
+    logging_dir = prepare_logging_dir(
+        finetuning_path_manager=finetuning_path_manager,
+        logger=logger,
+    )
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # Finetuning setup
+
+    training_args = prepare_training_args(
+        finetuning_config=finetuning_config,
+        finetuned_model_dir=finetuned_model_dir,
+        logging_dir=logging_dir,
+    )
+
+    trainer = transformers.Trainer(
+        model=base_model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=train_dataset_mapped,  # type: ignore
+        eval_dataset=eval_dataset_mapped,  # type: ignore
+        tokenizer=tokenizer,
+    )
+
+    finetune_model(
+        trainer=trainer,
+        logger=logger,
+    )
+
+    save_tuned_model(
+        trainer=trainer,
+        logger=logger,
+    )
+
+    evaluate_tuned_model(
+        trainer=trainer,
+        logger=logger,
+    )
+
+    return None
+
+
+def load_base_model(
+    finetuning_config: FinetuningConfig,
+    device: torch.device,
+    logger: logging.Logger = logging.getLogger(__name__),
+):
     logger.info(
         f"Loading model " f"{finetuning_config.pretrained_model_name_or_path = } ..."
     )
@@ -187,12 +261,79 @@ def run_finetuning(
     )
     logger.info(f"Moving model to {device = } DONE")
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Tokenize data using the Dataset.map() function
+    return model
 
+
+def prepare_finetuned_model_dir(
+    finetuning_path_manager: FinetuningPathManager,
+    logger: logging.Logger = logging.getLogger(__name__),
+) -> os.PathLike:
+    finetuned_model_dir = finetuning_path_manager.finetuned_model_dir
+    logger.info(f"{finetuned_model_dir = }")
+
+    # Create the output directory if it does not exist
+    finetuned_model_dir.mkdir(
+        parents=True,
+        exist_ok=True,
+    )
+
+    return finetuned_model_dir
+
+
+def prepare_logging_dir(
+    finetuning_path_manager: FinetuningPathManager,
+    logger: logging.Logger = logging.getLogger(__name__),
+) -> os.PathLike | None:
+    logging_dir = finetuning_path_manager.logging_dir
+    logger.info(f"{logging_dir = }")
+
+    # Create the logging directory if it does not exist
+    if logging_dir is not None:
+        logging_dir.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+    else:
+        logger.info(
+            f"No logging directory specified. "
+            f"Using default logging from transformers.Trainer."
+        )
+
+    return logging_dir
+
+
+def finetune_model(
+    trainer: transformers.Trainer,
+    logger: logging.Logger = logging.getLogger(__name__),
+) -> None:
+    logger.info(f"Calling trainer.train() ...")
+
+    training_call_output = trainer.train(
+        resume_from_checkpoint=False,
+    )
+
+    logger.info(f"Calling trainer.train() DONE")
+
+    logger.info(f"training_call_output:\n" f"{training_call_output}")
+
+    return None
+
+
+def prepare_model_input(
+    train_dataset: datasets.Dataset,
+    eval_dataset: datasets.Dataset,
+    tokenizer: transformers.PreTrainedTokenizer | transformers.PreTrainedTokenizerFast,
+    finetuning_config: FinetuningConfig,
+) -> tuple[
+    datasets.Dataset,
+    datasets.Dataset,
+]:
     def tokenize_function(
-        dataset_entries,
-    ):
+        dataset_entries: dict[
+            str,
+            list,
+        ],
+    ) -> transformers.tokenization_utils_base.BatchEncoding:
         # NOTE: This assumes that train and eval datasets use the same column name
         column_name = finetuning_config.finetuning_datasets.train_dataset.column_name
 
@@ -214,47 +355,39 @@ def run_finetuning(
         batched=True,
     )
 
-    # Data collator
-    data_collator = transformers.DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        mlm=True,
-        mlm_probability=finetuning_config.mlm_probability,
+    return train_dataset_mapped, eval_dataset_mapped
+
+
+def load_tokenizer(
+    finetuning_config: FinetuningConfig,
+    logger: logging.Logger = logging.getLogger(__name__),
+) -> transformers.PreTrainedTokenizer | transformers.PreTrainedTokenizerFast:
+    # ? Do we need other config arguments for the tokenizer for finetuning here?
+
+    logger.info(
+        f"Loading tokenizer "
+        f"{finetuning_config.pretrained_model_name_or_path = } ..."
     )
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Output path
-    finetuning_path_manager = get_finetuning_path_manager(
-        config=main_config,
-        logger=logger,
+    tokenizer = AutoTokenizer.from_pretrained(
+        pretrained_model_name_or_path=finetuning_config.pretrained_model_name_or_path,
     )
-
-    finetuned_model_dir = finetuning_path_manager.finetuned_model_dir
-    logger.info(f"{finetuned_model_dir = }")
-
-    # Create the output directory if it does not exist
-    finetuned_model_dir.mkdir(
-        parents=True,
-        exist_ok=True,
+    logger.info(
+        f"Loading tokenizer "
+        f"{finetuning_config.pretrained_model_name_or_path = } DONE"
     )
+    logger.info(f"tokenizer:\n{tokenizer}")
 
-    logging_dir = finetuning_path_manager.logging_dir
-    logger.info(f"{logging_dir = }")
+    # Make sure not to accidentally modify the tokenizer pad token (tokenizer.pad_token) here.
+    # In particular, it is not custom to set the pad token to the eos token for masked language model training.
 
-    # Create the logging directory if it does not exist
-    if logging_dir is not None:
-        logging_dir.mkdir(
-            parents=True,
-            exist_ok=True,
-        )
-    else:
-        logger.info(
-            f"No logging directory specified. "
-            f"Using default logging from transformers.Trainer."
-        )
+    return tokenizer
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Finetuning setup
 
+def prepare_training_args(
+    finetuning_config: FinetuningConfig,
+    finetuned_model_dir: os.PathLike,
+    logging_dir: os.PathLike | None = None,
+):
     training_args = transformers.TrainingArguments(
         output_dir=str(finetuned_model_dir),
         overwrite_output_dir=True,
@@ -280,46 +413,33 @@ def run_finetuning(
         seed=finetuning_config.seed,
     )
 
-    trainer = transformers.Trainer(
-        model=model,
-        args=training_args,
-        data_collator=data_collator,
-        train_dataset=train_dataset_mapped,  # type: ignore
-        eval_dataset=eval_dataset_mapped,  # type: ignore
-        tokenizer=tokenizer,
-    )
+    return training_args
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Train the model
 
-    logger.info(f"Calling trainer.train() ...")
-
-    # Train the model
-    #
-    training_call_output = trainer.train(
-        resume_from_checkpoint=False,
-    )
-
-    logger.info(f"Calling trainer.train() DONE")
-
-    logger.info(f"training_call_output:\n{training_call_output}")
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Save the model
-
+def save_tuned_model(
+    trainer: transformers.Trainer,
+    logger: logging.Logger = logging.getLogger(__name__),
+) -> None:
     logger.info(f"Calling trainer.save_model() ...")
     trainer.save_model()
     logger.info(f"Calling trainer.save_model() DONE")
 
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    # Evaluate the model
+    return None
 
+
+def evaluate_tuned_model(
+    trainer: transformers.Trainer,
+    logger: logging.Logger = logging.getLogger(__name__),
+) -> None:
     logger.info(f"Evaluating the model ...")
+
     eval_results = trainer.evaluate()
 
     perplexity = math.exp(eval_results["eval_loss"])
     logger.info(f"perplexity:\n{perplexity:.2f}")
     logger.info(f"eval_results:\n{eval_results}")
+
+    logger.info(f"Evaluating the model DONE")
 
     return None
 
