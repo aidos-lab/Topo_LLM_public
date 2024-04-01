a9b83e7407afa35e9167ae5f75fb980cd68e59b3
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-04-01 14:38:11 +0200

    Beginning to move the data split and truncation into separate config group; writing simple tests for validating examples of the main config, data config and finetuning config.

---
+++ tests/config_classes/test_DataConfig.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+#
+# Copyright 2024
+# Heinrich Heine University Dusseldorf,
+# Faculty of Mathematics and Natural Sciences,
+# Computer Science Department
+#
+# Authors:
+# Benjamin Ruppik (ruppik@hhu.de)
+# Julius von Rohrscheidt (julius.rohrscheidt@helmholtz-muenchen.de)
+#
+# Code generation tools and workflows:
+# First versions of this code were potentially generated
+# with the help of AI writing assistants including
+# GitHub Copilot, ChatGPT, Microsoft Copilot, Google Gemini.
+# Afterwards, the generated segments were manually reviewed and edited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+import pprint
+
+from hydra import compose, initialize, initialize_config_module
+import omegaconf
+
+from topollm.config_classes.MainConfig import DataConfig
+
+logger = logging.getLogger(__name__)
+
+
+def test_hydra_with_DataConfig() -> None:
+    with initialize(
+        config_path="../../configs/data/",
+        version_base=None,
+    ):
+        # config is relative to a module
+        cfg: omegaconf.DictConfig = compose(
+            config_name="bbc",
+            overrides=[
+                "dataset_description_string=overwritten_dataset_desc",
+            ],
+        )
+
+        logger.info(f"cfg:\n" f"{pprint.pformat(cfg)}")
+
+        # This tests whether the configuration is valid
+        data_config = DataConfig.model_validate(
+            obj=cfg,
+        )
+
+        logger.info(f"data_config:\n" f"{pprint.pformat(data_config)}")
+
+    return None
