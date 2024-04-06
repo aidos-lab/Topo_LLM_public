6d7bcbd14f92abf315756a1e98bfbd658c6d2e0f
ruppik <ruppik@hhu.de>
2024-04-06 13:55:21 +0000

    Made the layer specification in the multirun compute embeddings script more flexible

--- topollm/compute_embeddings/run_multiple_compute_embeddings.sh
+++ topollm/compute_embeddings/run_multiple_compute_embeddings.sh
@@ -9,21 +9,30 @@ PYTHON_SCRIPT_NAME="run_compute_embeddings.py"
 
 DATA_LIST="bbc,multiwoz21,sgd,wikitext"
 
-LANGUAGE_MODEL_LIST="bert-base-uncased,roberta-base"
+# LANGUAGE_MODEL_LIST="bert-base-uncased,roberta-base"
+LANGUAGE_MODEL_LIST="roberta-base,roberta-base_finetuned-on-multiwoz21_ftm-lora"
 
-LAYER_INDICES_LIST="0,1,2,3,4"
+LAYER_INDICES_LIST="[0],[1],[2],[3],[4],[5],[6],[7],[8],[9],[10],[11]"
 
 # ==================================================== #
 
 ADDITIONAL_OVERRIDES=""
-# ADDITIONAL_OVERRIDES="data.number_of_samples=100"
+ADDITIONAL_OVERRIDES="data.number_of_samples=100"
 
-# Note that the syntax "layer_indices.0" is used to specify the first element
-# of the list "layer_indices" in the configuration file.
-# https://stackoverflow.com/questions/77940195/overriding-list-entry-in-hydra-from-another-yaml
 python3 $PYTHON_SCRIPT_NAME \
     --multirun \
     data=$DATA_LIST \
     language_model=$LANGUAGE_MODEL_LIST \
-    embeddings.embedding_extraction.layer_indices.0=$LAYER_INDICES_LIST \
+    embeddings.embedding_extraction.layer_indices=$LAYER_INDICES_LIST \
     $ADDITIONAL_OVERRIDES
+
+# Alternative way to specify the list of layer indices override:
+#
+# Define the list of layer indices as a string:
+# > LAYER_INDICES_LIST="0,1,2,3,4"
+# The in the run command, add the following line:
+# > embeddings.embedding_extraction.layer_indices=$LAYER_INDICES_LIST
+#
+# Note that the syntax "layer_indices.0" is used to specify the first element
+# of the list "layer_indices" in the configuration file.
+# https://stackoverflow.com/questions/77940195/overriding-list-entry-in-hydra-from-another-yaml
