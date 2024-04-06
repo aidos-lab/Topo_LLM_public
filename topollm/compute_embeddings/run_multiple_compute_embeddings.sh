8c01f68caaf21bbee6fa54fcaa2755d936400834
ruppik <ruppik@hhu.de>
2024-04-06 13:36:51 +0000

    Updated the multirun scripts to makee them more flexible

--- topollm/compute_embeddings/run_multiple_compute_embeddings.sh
+++ topollm/compute_embeddings/run_multiple_compute_embeddings.sh
@@ -4,16 +4,23 @@
 
 PYTHON_SCRIPT_NAME="run_compute_embeddings.py"
 
+# ==================================================== #
+# Select the parameters here
+
 DATA_LIST="bbc,multiwoz21,sgd,wikitext"
 
 LANGUAGE_MODEL_LIST="bert-base-uncased,roberta-base"
 
 LAYER_INDICES_LIST="0,1,2,3,4"
 
+# ==================================================== #
+
 ADDITIONAL_OVERRIDES=""
-# ADDITIONAL_OVERRIDES="finetuning.max_steps=10"
+# ADDITIONAL_OVERRIDES="data.number_of_samples=100"
 
-
+# Note that the syntax "layer_indices.0" is used to specify the first element
+# of the list "layer_indices" in the configuration file.
+# https://stackoverflow.com/questions/77940195/overriding-list-entry-in-hydra-from-another-yaml
 python3 $PYTHON_SCRIPT_NAME \
     --multirun \
     data=$DATA_LIST \
--- topollm/model_inference/run_multiple_inference.sh
+++ topollm/model_inference/run_multiple_inference.sh
@@ -4,18 +4,30 @@
 
 PYTHON_SCRIPT_NAME="run_inference_pipeline.py"
 
-LANGUAGE_MODEL_LIST="bert-base-uncased,gpt2-large,roberta-base"
+# ==================================================== #
+# Select the parameters here
+
+# LANGUAGE_MODEL_LIST="bert-base-uncased,gpt2-large,roberta-base"
+LANGUAGE_MODEL_LIST="roberta-base,roberta-base_finetuned-on-multiwoz21_ftm-lora"
 
 ADDITIONAL_OVERRIDES=""
 # ADDITIONAL_OVERRIDES="finetuning.max_steps=10"
+
+# ==================================================== #
 
 # NOTE:
 # With torch backend set to "mps" on MacBook M1,
 # the inference of the "gpt2-large" model appears to be broken.
 # This is why we set the preferred_torch_backend to "cpu" here.
 
+# PREFERRED_TORCH_BACKEND="auto"
+PREFERRED_TORCH_BACKEND="cpu"
+
 python3 $PYTHON_SCRIPT_NAME \
     --multirun \
     language_model=$LANGUAGE_MODEL_LIST \
-    preferred_torch_backend=cpu \
+    preferred_torch_backend=$PREFERRED_TORCH_BACKEND \
     $ADDITIONAL_OVERRIDES
+
+# Exit with status code 0
+exit 0
