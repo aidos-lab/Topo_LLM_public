c1fd2b7f9882aff42fb7332d3163c07f7a939374
ruppik <ruppik@hhu.de>
2024-04-11 13:14:09 +0000

    Updated the multiple finetuning running script to work with the new config groups for the base_model config

--- topollm/model_finetuning/run_multiple_finetunings.sh
+++ topollm/model_finetuning/run_multiple_finetunings.sh
@@ -7,13 +7,16 @@ PYTHON_SCRIPT_NAME="run_finetune_masked_language_model_on_huggingface_dataset.py
 # ==================================================== #
 # Select the parameters here
 
-# PRETRAINED_MODEL_NAME_OR_PATH_LIST="bert-base-uncased,roberta-base"
-# PRETRAINED_MODEL_NAME_OR_PATH_LIST="bert-base-uncased"
-PRETRAINED_MODEL_NAME_OR_PATH_LIST="roberta-base"
+BASE_MODEL_LIST="gpt2-medium"
+# BASE_MODEL_LIST="roberta-base"
+# BASE_MODEL_LIST="bert-base-uncased,roberta-base"
 
 # FINETUNING_DATASETS_LIST="train_and_eval_on_bbc,train_and_eval_on_iclr_2024_submissions,train_and_eval_on_multiwoz21,train_and_eval_on_sgd,train_and_eval_on_wikitext"
 # FINETUNING_DATASETS_LIST="train_and_eval_on_iclr_2024_submissions"
 FINETUNING_DATASETS_LIST="train_and_eval_on_multiwoz21"
+
+LR_SCHEDULER_TYPE="linear"
+# LR_SCHEDULER_TYPE="constant"
 
 # ADDITIONAL_OVERRIDES=""
 # ADDITIONAL_OVERRIDES="finetuning.max_steps=10"
@@ -22,9 +25,9 @@ FINETUNING_DATASETS_LIST="train_and_eval_on_multiwoz21"
 
 python3 $PYTHON_SCRIPT_NAME \
     --multirun \
-    finetuning.pretrained_model_name_or_path=$PRETRAINED_MODEL_NAME_OR_PATH_LIST \
-    finetuning.num_train_epochs=50 \
-    finetuning.lr_scheduler_type=constant \
+    finetuning/base_model@finetuning=$BASE_MODEL_LIST \
+    finetuning.num_train_epochs=5 \
+    finetuning.lr_scheduler_type=$LR_SCHEDULER_TYPE \
     finetuning/finetuning_datasets=$FINETUNING_DATASETS_LIST \
     finetuning/peft=standard,lora \
     $ADDITIONAL_OVERRIDES
