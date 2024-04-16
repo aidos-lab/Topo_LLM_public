de05e4caae398708adec08c89fc305c32aeacb32
ruppik <ruppik@hhu.de>
2024-04-16 21:19:08 +0000

    Added ruff config to pyproject.toml; updated small parts of topollm.compute_embeddings.data_prep to conform with ruff guidelines

--- topollm/compute_embeddings/run_multiple_compute_embeddings.sh
+++ topollm/compute_embeddings/run_multiple_compute_embeddings.sh
@@ -9,12 +9,13 @@ PYTHON_SCRIPT_NAME_DATA_PREP="data_prep.py"
 # Select the parameters here
 
 # DATA_LIST="bbc,multiwoz21,sgd,wikitext"
-#DATA_LIST="wikitext"
+# DATA_LIST="wikitext"
 DATA_LIST="multiwoz21_validation,iclr_2024_submissions,wikitext"
 
 # LANGUAGE_MODEL_LIST="bert-base-uncased,roberta-base"
-LANGUAGE_MODEL_LIST="roberta-base,roberta-base_finetuned-on-multiwoz21_ftm-lora"
-#LANGUAGE_MODEL_LIST="roberta-base"
+# LANGUAGE_MODEL_LIST="roberta-base,roberta-base_finetuned-on-multiwoz21_ftm-lora"
+# LANGUAGE_MODEL_LIST="roberta-base"
+LANGUAGE_MODEL_LIST="gpt2-medium,gpt2-medium_finetuned-on-multiwoz21_ftm-standard,roberta-base_finetuned-on-multiwoz21_ftm-standard_overfitted"
 
 #LAYER_INDICES_LIST="[-1],[-2]"
 LAYER_INDICES_LIST="[-1],[-2],[-3],[-4],[-5],[-6],[-7],[-8],[-9],[-10],[-11],[-12]"
@@ -24,6 +25,7 @@ DATA_PREP_SAMPLES="10000,20000,50000"
 
 # ADDITIONAL_OVERRIDES=""
 ADDITIONAL_OVERRIDES="data.number_of_samples=5000"
+
 # ==================================================== #
 
 
@@ -41,14 +43,3 @@ python3 $PYTHON_SCRIPT_NAME_DATA_PREP \
     data_prep_samples=$DATA_PREP_SAMPLES \
     embeddings.embedding_extraction.layer_indices=$LAYER_INDICES_LIST \
     $ADDITIONAL_OVERRIDES
-
-# Alternative way to specify the list of layer indices override:
-#
-# Define the list of layer indices as a string:
-# > LAYER_INDICES_LIST="0,1,2,3,4"
-# The in the run command, add the following line:
-# > embeddings.embedding_extraction.layer_indices=$LAYER_INDICES_LIST
-#
-# Note that the syntax "layer_indices.0" is used to specify the first element
-# of the list "layer_indices" in the configuration file.
-# https://stackoverflow.com/questions/77940195/overriding-list-entry-in-hydra-from-another-yaml
