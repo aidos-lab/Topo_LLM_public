eb1e8ab5f02bad1cf6c6131e21fea96156d4a7ef
ruppik <ruppik@hhu.de>
2024-05-02 18:01:00 +0000

    Adding the option to directly overwrite the CUDA_VISIBLE_DEVICES environement variable into the finetuning script

---
+++ topollm/model_inference/perplexity/run_multiple_compute_perplexity_with_selected_data.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# https://hydra.cc/docs/tutorials/basic/running_your_app/multi-run/
+
+PYTHON_SCRIPT_NAME="run_compute_perplexity.py"
+
+# ==================================================== #
+# Select the parameters here
+
+LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard"
+
+DATA_LIST="one-year-of-tsla-on-reddit_validation"
+
+ADDITIONAL_OVERRIDES="data.number_of_samples=3000"
+ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200,1600,2000,2400,2800"
+
+
+# DATA_LIST="one-year-of-tsla-on-reddit"
+DATA_LIST="one-year-of-tsla-on-reddit,one-year-of-tsla-on-reddit_validation,multiwoz21_validation,sgd,iclr_2024_submissions,wikitext"
+# DATA_LIST="one-year-of-tsla-on-reddit,one-year-of-tsla-on-reddit_validation"
+# DATA_LIST="bbc,multiwoz21,sgd,wikitext"
+# DATA_LIST="one-year-of-tsla-on-reddit_validation,multiwoz21_validation,sgd,iclr_2024_submissions,wikitext"
+# DATA_LIST="multiwoz21_validation"
+
+
+# LANGUAGE_MODEL_LIST="roberta-base,roberta-base_finetuned-on-multiwoz21_ftm-lora"
+
+ADDITIONAL_OVERRIDES=""
+ADDITIONAL_OVERRIDES="data.number_of_samples=3000"
+ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200,1600,2000,2400,2800"
+
+# ==================================================== #
+
+python3 $PYTHON_SCRIPT_NAME \
+    --multirun \
+    data=$DATA_LIST \
+    language_model=$LANGUAGE_MODEL_LIST \
+    $ADDITIONAL_OVERRIDES
+
+# Exit with status code 0
+exit 0
