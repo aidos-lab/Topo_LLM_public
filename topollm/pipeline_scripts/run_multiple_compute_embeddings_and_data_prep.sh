bce734dc34a821c4efd4fe301efa01ce84317676
ruppik <ruppik@hhu.de>
2024-05-13 12:36:42 +0000

    Adding config files for fine-tuned model with frozen layers

--- topollm/pipeline_scripts/run_multiple_compute_embeddings_and_data_prep.sh
+++ topollm/pipeline_scripts/run_multiple_compute_embeddings_and_data_prep.sh
@@ -2,7 +2,7 @@
 
 # https://hydra.cc/docs/tutorials/basic/running_your_app/multi-run/
 
-PYTHON_SCRIPT_NAME="../compute_embeddings/run_compute_embeddings.py"
+PYTHON_SCRIPT_NAME_COMPUTE_EMBEDDINGS="../compute_embeddings/run_compute_embeddings.py"
 PYTHON_SCRIPT_NAME_DATA_PREP="../embeddings_data_prep/run_embeddings_data_prep.py"
 
 # ==================================================== #
@@ -24,8 +24,9 @@ DATA_LIST="one-year-of-tsla-on-reddit_validation"
 # LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-multiwoz21_ftm-standard_overfitted"
 # LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard"
 # LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard_overfitted"
+LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard_freeze-first-6-layers_overfitted"
 
-LANGUAGE_MODEL_LIST="gpt2-medium"
+# LANGUAGE_MODEL_LIST="gpt2-medium"
 # LANGUAGE_MODEL_LIST="gpt2-medium_finetuned-on-multiwoz21_ftm-standard"
 # LANGUAGE_MODEL_LIST="gpt2-medium_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard"
 
@@ -35,11 +36,15 @@ LANGUAGE_MODEL_LIST="gpt2-medium"
 
 # Use the following line to process only the last layer.
 # LAYER_INDICES_LIST="[-1]"
+
+LAYER_INDICES_LIST="[-1],[-2]"
+
 # Note that "roberta-base" has 12 layers.
 # LAYER_INDICES_LIST="[-1],[-2],[-3],[-4],[-5],[-6],[-7],[-8],[-9],[-10],[-11],[-12]"
+
 # Note that "gpt2-medium" has 24 layers.
 # LAYER_INDICES_LIST="[-1],[-3],[-5],[-7],[-9],[-11],[-13],[-15],[-17],[-19],[-21],[-23]"
-LAYER_INDICES_LIST="[-1],[-2],[-3],[-4],[-5],[-6],[-7],[-8],[-9],[-10],[-11],[-12],[-13],[-14],[-15],[-16],[-17],[-18],[-19],[-20],[-21],[-22],[-23],[-24]"
+# LAYER_INDICES_LIST="[-1],[-2],[-3],[-4],[-5],[-6],[-7],[-8],[-9],[-10],[-11],[-12],[-13],[-14],[-15],[-16],[-17],[-18],[-19],[-20],[-21],[-22],[-23],[-24]"
 
 EMBEDDINGS_DATA_PREP_NUM_SAMPLES="30000"
 # EMBEDDINGS_DATA_PREP_NUM_SAMPLES="50000"
@@ -52,20 +57,23 @@ ADDITIONAL_OVERRIDES="data.number_of_samples=3000"
 # ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200"
 # ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200,1600,2000,2400,2800"
 # ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200,1600,2000,2400,2800,3200,3600,4000,4400,4800,5200,5600,6000,6400,6800,7200,7600,8000,8400,8800,9200,9600,10000,10400,10800,11200,11600,12000,12400,12800,13200,13600,14000,14400,14800,15200,15600"
-# ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200,1600,2000,2400,2800,3200,3600,4000,4400,4800,5200,5600,6000,6400,6800,7200,7600,8000,8400,8800,9200,9600,10000,10400,10800,11200,11600,12000,12400,12800,13200,13600,14000,14400,14800,15200,15600,16000,16400,16800,17200,17600,18000,18400,18800,19200,19600,20000,20400,20800,21200,21600,22000,22400,22800,23200,23600,24000,24400,24800,25200,25600,26000,26400,26800,27200,27600,28000,28400,28800,29200,29600,30000,30400,30800,31200"
+ADDITIONAL_OVERRIDES+=" language_model.checkpoint_no=400,800,1200,1600,2000,2400,2800,3200,3600,4000,4400,4800,5200,5600,6000,6400,6800,7200,7600,8000,8400,8800,9200,9600,10000,10400,10800,11200,11600,12000,12400,12800,13200,13600,14000,14400,14800,15200,15600,16000,16400,16800,17200,17600,18000,18400,18800,19200,19600,20000,20400,20800,21200,21600,22000,22400,22800,23200,23600,24000,24400,24800,25200,25600,26000,26400,26800,27200,27600,28000,28400,28800,29200,29600,30000,30400,30800,31200"
 
-CUDA_VISIBLE_DEVICES=0
+CUDA_VISIBLE_DEVICES=1
 
 # ==================================================== #
 
+echo "Calling python scripts."
 
-python3 $PYTHON_SCRIPT_NAME \
+python3 $PYTHON_SCRIPT_NAME_COMPUTE_EMBEDDINGS \
     --multirun \
     data=$DATA_LIST \
     language_model=$LANGUAGE_MODEL_LIST \
     embeddings.embedding_extraction.layer_indices=$LAYER_INDICES_LIST \
     hydra.job.env_set.CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES \
     $ADDITIONAL_OVERRIDES
+
+echo "Finished running the first script."
 
 python3 $PYTHON_SCRIPT_NAME_DATA_PREP \
     --multirun \
@@ -75,6 +83,9 @@ python3 $PYTHON_SCRIPT_NAME_DATA_PREP \
     embeddings_data_prep.num_samples=$EMBEDDINGS_DATA_PREP_NUM_SAMPLES \
     $ADDITIONAL_OVERRIDES
 
+echo "Finished running the second script."
+
 # ==================================================== #
 
+echo "Finished running the scripts."
 exit 0
