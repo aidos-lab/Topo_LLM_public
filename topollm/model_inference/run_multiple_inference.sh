e8e863deb138510ec387a833e17519cf7c4498eb
ruppik <ruppik@hhu.de>
2024-05-21 22:13:38 +0000

    Added config file for model fine-tuned on whole multiwoz-train dataset

--- topollm/model_inference/run_multiple_inference.sh
+++ topollm/model_inference/run_multiple_inference.sh
@@ -7,11 +7,12 @@ PYTHON_SCRIPT_NAME="run_inference_pipeline.py"
 # ==================================================== #
 # Select the parameters here
 
+LANGUAGE_MODEL_LIST="roberta-base"
 # LANGUAGE_MODEL_LIST="bert-base-uncased,gpt2-large,roberta-base"
 # LANGUAGE_MODEL_LIST="roberta-base,roberta-base_finetuned-on-multiwoz21_ftm-lora"
 # LANGUAGE_MODEL_LIST="bert-base-uncased,bert-base-uncased_finetuned-on-xsum_ftm-standard,bert-base-uncased_finetuned-on-multiwoz21_ftm-standard,bert-base-uncased_finetuned-on-sgd_ftm-lora"
-LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard_freeze-first-6-layers_overfitted"
-
+# LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-one-year-of-tsla-on-reddit_ftm-standard_freeze-first-6-layers_overfitted"
+# LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-multiwoz21_ftm-standard_full-dataset"
 
 ADDITIONAL_OVERRIDES=""
 
