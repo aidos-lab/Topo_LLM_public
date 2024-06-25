9fe7c95dc90f62e009b8a2bd1ce89c3fe48fa35d
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-06-25 11:10:39 +0200

    Adding feature flags to the finetuning script to allow skipping the expensive finetuning process

--- topollm/model_finetuning/run_finetune_language_model_on_huggingface_dataset.py
+++ topollm/model_finetuning/run_finetune_language_model_on_huggingface_dataset.py
@@ -29,7 +29,6 @@
 
 import logging
 import os
-import pathlib
 from typing import TYPE_CHECKING
 
 import hydra
@@ -44,6 +43,7 @@ from topollm.config_classes.setup_OmegaConf import setup_OmegaConf
 from topollm.logging.initialize_configuration_and_log import initialize_configuration
 from topollm.logging.setup_exception_logging import setup_exception_logging
 from topollm.model_finetuning.do_finetuning_process import do_finetuning_process
+from topollm.model_finetuning.initialize_wandb import initialize_wandb
 from topollm.model_finetuning.prepare_finetuned_model_dir import prepare_finetuned_model_dir
 from topollm.model_handling.get_torch_device import get_torch_device
 from topollm.path_management.finetuning.factory import get_finetuning_path_manager
@@ -55,6 +55,9 @@ if TYPE_CHECKING:
 # Increase the wandb service wait time to prevent errors.
 # https://github.com/wandb/wandb/issues/5214
 os.environ["WANDB__SERVICE_WAIT"] = "300"
+wandb.require(
+    "core",
+)
 
 global_logger = logging.getLogger(__name__)
 
@@ -87,47 +90,18 @@ def main(
         logger=logger,
     )
 
-    wandb_dir = pathlib.Path(
-        main_config.wandb.dir,
-    )
-    logger.info(
-        f"{wandb_dir = }",  # noqa: G004 - low overhead
-    )
-    # Create the wandb directory if it does not exist
-    wandb_dir.mkdir(
-        parents=True,
-        exist_ok=True,
-    )
-
-    wandb.init(
-        dir=main_config.wandb.dir,
-        entity=main_config.wandb.entity,  # Note: To make this None, use null in the hydra config
-        project=main_config.wandb.project,
-        settings=wandb.Settings(
-            start_method="thread",  # Note: https://docs.wandb.ai/guides/integrations/hydra#troubleshooting-multiprocessing
-            _service_wait=300,
-            init_timeout=300,
-        ),
-        tags=main_config.wandb.tags,
-    )
-
-    # Note: Convert OmegaConf to dict to avoid issues with wandb
-    # https://docs.wandb.ai/guides/integrations/hydra#track-hyperparameters
-    omegaconf_converted_to_dict = omegaconf.OmegaConf.to_container(
-        cfg=config,
-        resolve=True,
-        throw_on_missing=True,
-    )
-
-    # Add the hydra config to the wandb config
-    # (so that they are tracked in the wandb run)
-    wandb.config.hydra = omegaconf_converted_to_dict
-
-    # Add information about the wandb run to the logger
-    if wandb.run is not None:
-        logger.info(
-            f"{wandb.run.dir = }",  # noqa: G004 - low overhead
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+    # Initialize wandb
+    if main_config.feature_flags.use_wandb:
+        initialize_wandb(
+            main_config=main_config,
+            config=config,
+            logger=logger,
         )
+    else:
+        os.environ["WANDB_MODE"] = "disabled"
+        # Note: Do not set `os.environ["WANDB_DISABLED"] = "true"` because this will raise the error
+        # `RuntimeError: WandbCallback requires wandb to be installed. Run `pip install wandb`.`
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     # Use accelerator if available
@@ -137,22 +111,25 @@ def main(
         logger=logger,
     )
 
-    logger.info(
-        "Calling do_finetuning_process ...",
-    )
+    if not main_config.feature_flags.skip_finetuning:
+        logger.info(
+            "Calling do_finetuning_process ...",
+        )
 
-    do_finetuning_process(
-        main_config=main_config,
-        device=device,
-        logger=logger,
-    )
+        do_finetuning_process(
+            main_config=main_config,
+            device=device,
+            logger=logger,
+        )
 
-    logger.info(
-        "Calling do_finetuning_process DONE",
-    )
+        logger.info(
+            "Calling do_finetuning_process DONE",
+        )
 
-    # We need to manually finish the wandb run so that the hydra multi-run submissions are not summarized in the same run
-    wandb.finish()
+    if main_config.feature_flags.use_wandb:
+        # We need to manually finish the wandb run
+        # so that the hydra multi-run submissions are not summarized in the same run
+        wandb.finish()
 
     logger.info(
         "Running script DONE",
