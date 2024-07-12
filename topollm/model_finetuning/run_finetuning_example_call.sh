af7184d450cbb4f8624dec405bb2d148ac2e1f93
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-07-13 01:58:01 +0200

    Adding a script for calling the finetuning job for debugging

--- topollm/model_finetuning/run_finetuning_example_call.sh
+++ topollm/model_finetuning/run_finetuning_example_call.sh
@@ -8,11 +8,11 @@ PYTHON_SCRIPT_PATH="${TOPO_LLM_REPOSITORY_BASE_PATH}/topollm/model_finetuning/${
 poetry run python3 \
     $PYTHON_SCRIPT_NAME \
     finetuning=finetuning_for_token_classification \
-    finetuning.finetuning_datasets.train_dataset.number_of_samples=1000 \
+    finetuning.finetuning_datasets.train_dataset.number_of_samples=3000 \
     finetuning.finetuning_datasets.eval_dataset.number_of_samples=200 \
-    finetuning.eval_steps=10 \
+    finetuning.eval_steps=100 \
     +finetuning.trainer_modifier.mode=ADD_WANDB_PREDICTION_PROGRESS_CALLBACK \
-    +finetuning.trainer_modifier.frequency=10 \
+    +finetuning.trainer_modifier.frequency=100 \
     feature_flags.wandb.use_wandb=true \
     wandb.project=Topo_LLM_finetuning_for_token_classification_debug
     
