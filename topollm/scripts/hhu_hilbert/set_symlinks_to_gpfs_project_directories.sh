f0a71779d129aaee2f2e8fddb34fcce831c72341
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-09-05 16:30:33 +0200

    Adding a script for creating symlinks from the repository data directory to the gpfs project directory on HHU Hilbert

---
+++ topollm/scripts/hhu_hilbert/set_symlinks_to_gpfs_project_directories.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# This script makes the following directories in the project's data directory:
+# `data/models/setsumbt_checkpoints/`
+# `data/models/trippy_checkpoints/`
+# point to the corresponding folders containing the data in the 
+# `/gpfs/project/projects/dsml/data`
+# directory.
+
+# Check if TOPO_LLM_REPOSITORY_BASE_PATH is set
+if [[ -z "${TOPO_LLM_REPOSITORY_BASE_PATH}" ]]; then
+  echo "Error: TOPO_LLM_REPOSITORY_BASE_PATH is not set."
+  exit 1
+fi
+
+GPFS_PROJECT_DIR_DSML="/gpfs/project/projects/dsml"
+
+# Define target (actual data) and link (symlink) directories
+DATA_DIR_SETSUMBT="${GPFS_PROJECT_DIR_DSML}/data/data-exp-eriments-zetsumbt"
+LINK_DIR_SETSUMBT="${TOPO_LLM_REPOSITORY_BASE_PATH}/data/models/setsumbt_checkpoints"
+
+DATA_DIR_TRIPPY="${GPFS_PROJECT_DIR_DSML}/data/trippy-for-ben"
+LINK_DIR_TRIPPY="${TOPO_LLM_REPOSITORY_BASE_PATH}/data/models/trippy_checkpoints"
+
+# Check if the directories or symlinks already exist
+if [ -e "$LINK_DIR_SETSUMBT" ]; then
+  echo "Warning: Directory or symlink '$LINK_DIR_SETSUMBT' already exists. Exiting."
+  exit 1
+fi
+
+if [ -e "$LINK_DIR_TRIPPY" ]; then
+  echo "Warning: Directory or symlink '$LINK_DIR_TRIPPY' already exists. Exiting."
+  exit 1
+fi
+
+# Create symbolic links
+echo "Linking setsumbt_checkpoints in git-source directory to data directory ..."
+ln -s "$DATA_DIR_SETSUMBT" "$LINK_DIR_SETSUMBT"
+
+echo "Linking trippy_checkpoints in git-source directory to data directory ..."
+ln -s "$DATA_DIR_TRIPPY" "$LINK_DIR_TRIPPY"
+
+echo "Symbolic links created successfully."
