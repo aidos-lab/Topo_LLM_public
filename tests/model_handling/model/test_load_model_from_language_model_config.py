7f2d2bfa20257357ea06f9124f34f4e42d622841
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2024-11-27 15:18:36 +0100

    Removing old, obsolete language model configs which resulted from fine-tunings; adding test cases for the load_model_from_language_model_config function; implementing the creation of from_pretrained_kwargs parameters for the dropout case.

--- tests/model_handling/model/test_load_model_from_language_model_config.py
+++ tests/model_handling/model/test_load_model_from_language_model_config.py
@@ -26,34 +26,35 @@
 # limitations under the License.
 
 import logging
-import os
 
 import pytest
 import torch
 from transformers import PreTrainedModel
 
-from tests.model_handling.parameter_lists import (
-    example_pretrained_model_name_or_path_list,
-)
-from topollm.model_handling.model.load_model import load_model
+from topollm.config_classes.language_model.language_model_config import LanguageModelConfig
+from topollm.model_handling.model.load_model_from_language_model_config import load_model_from_language_model_config
+from topollm.typing.enums import Verbosity
 
 
-@pytest.mark.parametrize(
-    "pretrained_model_name_or_path",
-    example_pretrained_model_name_or_path_list,
-)
 @pytest.mark.uses_transformers_models
 def test_load_model(
-    pretrained_model_name_or_path: str | os.PathLike,
+    language_model_config: LanguageModelConfig,
     device_fixture: torch.device,
+    verbosity: Verbosity,
     logger_fixture: logging.Logger,
+    from_pretrained_kwargs_instance: dict | None = None,
 ) -> None:
     """Test the load_model function."""
-    model = load_model(
-        pretrained_model_name_or_path=pretrained_model_name_or_path,
+    model: PreTrainedModel = load_model_from_language_model_config(
+        language_model_config=language_model_config,
+        from_pretrained_kwargs_instance=from_pretrained_kwargs_instance,
         device=device_fixture,
-        verbosity=1,
+        verbosity=verbosity,
         logger=logger_fixture,
+    )
+
+    logger_fixture.info(
+        msg=f"Loaded model:\n{model}",  # noqa: G004 - low overhead
     )
 
     assert model is not None  # noqa: S101 - pytest assert
---
+++ topollm/model_handling/model/get_from_pretrained_kwargs_dict_for_dropout_parameters.py
@@ -0,0 +1,54 @@
+# Copyright 2024-2025
+# Heinrich Heine University Dusseldorf,
+# Faculty of Mathematics and Natural Sciences,
+# Computer Science Department
+#
+# Authors:
+# Benjamin Ruppik (mail@ruppik.net)
+# Julius von Rohrscheidt (julius.rohrscheidt@helmholtz-muenchen.de)
+#
+# Code generation tools and workflows:
+# First versions of this code were potentially generated
+# with the help of AI writing assistants including
+# GitHub Copilot, ChatGPT, Microsoft Copilot, Google Gemini.
+# Afterwards, the generated segments were manually reviewed and edited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from topollm.config_classes.language_model.language_model_config import LanguageModelConfig
+from topollm.typing.enums import DropoutMode
+
+
+def get_from_pretrained_kwargs_dict_for_dropout_parameters(
+    language_model_config: LanguageModelConfig,
+) -> dict:
+    """Get the keyword arguments for the dropout parameters."""
+    match language_model_config.dropout.mode:
+        case DropoutMode.DEFAULTS:
+            # Empty dictionary for default values.
+            from_pretrained_kwargs_dict: dict = {}
+        case DropoutMode.MODIFY_ROBERTA_DROPOUT_PARAMETERS:
+            from_pretrained_kwargs_dict: dict = {
+                "hidden_dropout_prob": language_model_config.dropout.probabilities.hidden_dropout_prob,
+                "attention_probs_dropout_prob": language_model_config.dropout.probabilities.attention_probs_dropout_prob,
+                "classifier_dropout": language_model_config.dropout.probabilities.classifier_dropout,
+            }
+        # Note: We can extend this switch statement with additional cases for other dropout modes
+        # if we add support for other language models later.
+        case _:
+            msg: str = f"Unknown {language_model_config.dropout.mode = }"
+            raise ValueError(
+                msg,
+            )
+
+    return from_pretrained_kwargs_dict
