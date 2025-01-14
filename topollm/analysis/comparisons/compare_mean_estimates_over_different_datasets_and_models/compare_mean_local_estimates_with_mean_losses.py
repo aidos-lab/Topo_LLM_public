3e40113fc0ab808ac215c1bcc9ead4c8bd7c1fd7
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-01-14 15:44:47 +0100

    Adding a placeholder script where we will implement the plot creation

---
+++ topollm/analysis/comparisons/compare_mean_estimates_over_different_datasets_and_models/compare_mean_local_estimates_with_mean_losses.py
@@ -0,0 +1,33 @@
+# Copyright 2024-2025
+# Heinrich Heine University Dusseldorf,
+# Faculty of Mathematics and Natural Sciences,
+# Computer Science Department
+#
+# Authors:
+# Benjamin Ruppik (mail@ruppik.net)
+# Julius von Rohrscheidt (julius.rohrscheidt@helmholtz-muenchen.de)
+#
+# Code generation tools and workflows:
+# First versions of this code were potentially generated
+# with the help of AI writing assistants including
+# GitHub Copilot, ChatGPT, Microsoft Copilot, Google Gemini.
+# Afterwards, the generated segments were manually reviewed and edited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# TODO: This script is under development.
+
+
+# TODO: Iterate over 'data/analysis/distances_and_influence_on_losses_and_local_estimates' directory
+# TODO: Load mean estimates and losses, and parse the corresponding model and dataset names
+# TODO: Make a scatter plot with mean estimates on the x-axis and mean losses on the y-axis
