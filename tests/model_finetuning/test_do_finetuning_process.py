Adding the gradient modifier information to the short model name, so that we can run fine-tunings with different freezing setups; adding a gradient modifier config for keeping the lm_head fixed.

tests/model_finetuning/test_do_finetuning_process.py
@@ -36,10 +36,10 @@ from topollm.config_classes.main_config import MainConfig
 from topollm.model_finetuning.do_finetuning_process import do_finetuning_process
 
 
-@pytest.mark.uses_transformers_models()
-@pytest.mark.high_memory_usage()
-@pytest.mark.slow()
-@pytest.mark.very_slow()
+@pytest.mark.uses_transformers_models
+@pytest.mark.high_memory_usage
+@pytest.mark.slow
+@pytest.mark.very_slow
 def test_do_finetuning_process(
     main_config: MainConfig,
     device_fixture: torch.device,