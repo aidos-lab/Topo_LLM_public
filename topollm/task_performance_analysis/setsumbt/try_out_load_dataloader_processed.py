8686ca295f22ae9becb30b5202296235b37c8f16
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-02-07 17:15:11 +0100

    Implementing the stacking function of the SetSUMBT saved dataloaders

--- topollm/task_performance_analysis/setsumbt/try_out_load_dataloader_processed.py
+++ topollm/task_performance_analysis/setsumbt/try_out_load_dataloader_processed.py
@@ -36,12 +36,14 @@ import hydra
 import hydra.core.hydra_config
 import omegaconf
 import torch
+import transformers
 
 from topollm.config_classes.constants import (
     HYDRA_CONFIGS_BASE_PATH,
 )
 from topollm.config_classes.setup_OmegaConf import setup_omega_conf
 from topollm.logging.initialize_configuration_and_log import initialize_configuration
+from topollm.logging.log_list_info import log_list_info
 from topollm.logging.log_recursive_dict_info import log_recursive_dict_info
 from topollm.logging.setup_exception_logging import setup_exception_logging
 from topollm.model_handling.prepare_loaded_model_container import (
@@ -177,29 +179,64 @@ def main(
     # > dataloader_processed["input_ids"][0][0]
 
     for sequence_to_decode in dataloader_processed["input_ids"][0]:
-        if verbosity >= Verbosity.NORMAL:
-            logger.info(
-                msg=f"{sequence_to_decode.shape = }",  # noqa: G004 - low overhead
-            )
-
-        # Decode the sequence
-        decoded_sequence = tokenizer.decode(
-            token_ids=sequence_to_decode,
+        decode_and_log_sequence(
+            sequence_to_decode=sequence_to_decode,
+            tokenizer=tokenizer,
+            verbosity=verbosity,
+            logger=logger,
         )
-
-        if verbosity >= Verbosity.NORMAL:
-            logger.info(
-                msg=f"{sequence_to_decode = }",  # noqa: G004 - low overhead
-            )
-            logger.info(
-                msg=f"{decoded_sequence = }",  # noqa: G004 - low overhead
-            )
 
     # # # #
     # Try out the function to concatenate the tensors of the individual dialogues and filter fully padded utterances
 
-    dataloader_concatenated = concatenate_tensors_from_dialogues_and_filter_fully_padded_utterances(
+    dataloader_stacked: dict = stack_tensors_from_dialogues_and_filter_fully_padded_utterances(
         dataloader_processed=dataloader_processed,
+    )
+
+    max_index: int = 100
+    decoded_sequences: list[str] = []
+    for sequence_to_decode, attention_sequence, dialogue_id in zip(
+        dataloader_stacked["input_ids"][:max_index],
+        dataloader_stacked["attention_mask"][:max_index],
+        dataloader_stacked["dialogue_ids"][:max_index],
+        strict=True,
+    ):
+        if verbosity >= Verbosity.NORMAL:
+            logger.info(
+                msg=20 * "-",
+            )
+            logger.info(
+                msg=f"{dialogue_id = }",  # noqa: G004 - low overhead
+            )
+
+        decoded_sequence = decode_and_log_sequence(
+            sequence_to_decode=sequence_to_decode,
+            tokenizer=tokenizer,
+            verbosity=verbosity,
+            logger=logger,
+        )
+        decoded_sequences.append(
+            decoded_sequence,
+        )
+
+        # Check that the attention_sequence is compatible with the sequence_to_decode
+        if attention_sequence.shape != sequence_to_decode.shape:
+            logger.warning(
+                msg=f"Attention sequence shape {attention_sequence.shape = } "  # noqa: G004 - low overhead
+                f"does not match sequence_to_decode shape {sequence_to_decode.shape = }",
+            )
+        # Check that the non-zero attention positions correspond to the non-padded positions in the sequence
+        # TODO: Implement this check
+
+        if verbosity >= Verbosity.NORMAL:
+            logger.info(
+                msg=20 * "-",
+            )
+
+    log_list_info(
+        list_=decoded_sequences,
+        list_name="decoded_sequences",
+        logger=logger,
     )
 
     logger.info(
@@ -207,24 +244,122 @@ def main(
     )
 
 
-def concatenate_tensors_from_dialogues_and_filter_fully_padded_utterances(
+def decode_and_log_sequence(
+    sequence_to_decode: torch.Tensor,
+    tokenizer: transformers.PreTrainedTokenizer | transformers.PreTrainedTokenizerFast,
+    verbosity: Verbosity = Verbosity.NORMAL,
+    logger: logging.Logger = default_logger,
+) -> str:
+    """Decode a sequence and log the result."""
+    if verbosity >= Verbosity.NORMAL:
+        logger.info(
+            msg=f"{sequence_to_decode.shape = }",  # noqa: G004 - low overhead
+        )
+
+    # Decode the sequence
+    decoded_sequence: str = tokenizer.decode(
+        token_ids=sequence_to_decode,
+    )
+
+    if verbosity >= Verbosity.NORMAL:
+        logger.info(
+            msg=f"{sequence_to_decode = }",  # noqa: G004 - low overhead
+        )
+        logger.info(
+            msg=f"{decoded_sequence = }",  # noqa: G004 - low overhead
+        )
+
+    return decoded_sequence
+
+
+def stack_tensors_from_dialogues_and_filter_fully_padded_utterances(
     dataloader_processed: dict,
+    verbosity: Verbosity = Verbosity.NORMAL,
+    logger: logging.Logger = default_logger,
 ) -> dict:
     """Concatenate the tensors of the individual dialogues and filter fully padded utterances.
 
     This function also replicates the dialogue-ids so that each utterance has a dialogue-id from which it originates.
     """
-
-    input_ids_to_concatenate = []
-    attention_masks_to_concatenate = []
+    input_ids_to_stack = []
+    attention_masks_to_stack = []
     dialogue_ids_to_concatenate = []
     # Iterate over the input_ids and attention_mask tensors for the dialogues and select only those utterances where the attention mask is non-zero
     #
     # Note the shapes:
     # > dataloader_processed["input_ids"].shape = torch.Size([8438, 12, 64])
-    # We want to iterate over the first dimension (8438), then from the second dimension (12) and select only those utterances where the attention mask is non-zero.
+    # We want to iterate over the first dimension (8438),
+    # then for each dialogue over the second dimension (12) with the turns,
+    # and select only those utterances where the attention mask is non-zero.
+    for index, (
+        dialogue_id,
+        input_ids_dialogue,
+        attention_mask,
+    ) in enumerate(
+        iterable=zip(
+            dataloader_processed["dialogue_ids"],
+            dataloader_processed["input_ids"],
+            dataloader_processed["attention_mask"],
+            strict=True,
+        ),
+    ):
+        for turn_index, (
+            input_ids_turn,
+            attention_mask_turn,
+        ) in enumerate(
+            iterable=zip(
+                input_ids_dialogue,
+                attention_mask,
+                strict=True,
+            ),
+        ):
+            if attention_mask_turn.sum() > 0:
+                input_ids_to_stack.append(
+                    input_ids_turn,
+                )
+                attention_masks_to_stack.append(
+                    attention_mask_turn,
+                )
+                dialogue_ids_to_concatenate.append(
+                    dialogue_id,
+                )
 
-    # TODO: Implement this function
+    if verbosity >= Verbosity.NORMAL:
+        logger.info(
+            msg=f"{len(input_ids_to_stack) = }",  # noqa: G004 - low overhead
+        )
+        logger.info(
+            msg=f"{len(attention_masks_to_stack) = }",  # noqa: G004 - low overhead
+        )
+        logger.info(
+            msg=f"{len(dialogue_ids_to_concatenate) = }",  # noqa: G004 - low overhead
+        )
+
+    # Stack the tensors of the individual turns
+    input_ids_concatenated: torch.Tensor = torch.stack(
+        tensors=input_ids_to_stack,
+        dim=0,
+    )
+    attention_masks_concatenated: torch.Tensor = torch.stack(
+        tensors=attention_masks_to_stack,
+        dim=0,
+    )
+
+    if verbosity >= Verbosity.NORMAL:
+        logger.info(
+            msg=f"{input_ids_concatenated.shape = }",  # noqa: G004 - low overhead
+        )
+        logger.info(
+            msg=f"{attention_masks_concatenated.shape = }",  # noqa: G004 - low overhead
+        )
+
+    result = {
+        "input_ids": input_ids_concatenated,
+        "attention_mask": attention_masks_concatenated,
+        "dialogue_ids": dialogue_ids_to_concatenate,
+    }
+
+    return result
 
 
 if __name__ == "__main__":
