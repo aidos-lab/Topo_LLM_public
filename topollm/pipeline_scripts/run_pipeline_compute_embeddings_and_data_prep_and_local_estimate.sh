44a29392c3c412d2c2ca268468a1b4d19222daa0
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-07-25 16:05:24 +0200

    Update launch configuration and scripts for Phi-3.5-mini-instruct model integration
    - Updating the topollm/pipeline_scripts/run_pipeline_compute_embeddings_and_data_prep_and_local_estimate.sh shell script for calling a debug setup with the Phi-3.5-mini-instruct model

--- topollm/pipeline_scripts/run_pipeline_compute_embeddings_and_data_prep_and_local_estimate.sh
+++ topollm/pipeline_scripts/run_pipeline_compute_embeddings_and_data_prep_and_local_estimate.sh
@@ -12,20 +12,38 @@ ABSOLUTE_PYTHON_SCRIPT_PATH="${TOPO_LLM_REPOSITORY_BASE_PATH}/${RELATIVE_PYTHON_
 #
 # We choose parameters here which can be used for debugging the script in a reasonable time.
 
+DATASET_TYPE_LINE=""
+# DATASET_TYPE_LINE="+data.dataset_type=huggingface_dataset_named_entity"
+
 # DATA_LIST="multiwoz21_validation,iclr_2024_submissions,wikitext"
 DATA_LIST="multiwoz21_validation"
 
-LANGUAGE_MODEL_LIST="roberta-base"
+# LANGUAGE_MODEL_LIST="roberta-base"
 # LANGUAGE_MODEL_LIST="roberta-base_finetuned-on-iclr_ftm-standard"
+LANGUAGE_MODEL_LIST="Phi-3.5-mini-instruct"
 
-CHECKPOINT_NO="400"
+CHECKPOINT_NO="-1"
+# CHECKPOINT_NO="400"
 # CHECKPOINT_NO="400,800,1200"
+
+# Notes: 
+# - For larger models, we need to reduce the batch size to avoid OOM errors on the GPU.
+EMBEDDINGS_BATCH_SIZE="2"
+# EMBEDDINGS_BATCH_SIZE="32" # We have used a batch size of 32 for RoBERTa-base and GPT-2-medium models.
+
+# Notes:
+# - The embedding dimension of the "Phi-3.5-mini-instruct" model is 3072.
+#   To save the array data of this model, we need to reduce the chunk size in the Zarr storage format to avoid errors like:
+#   "Codec does not support buffers of > 2147483647 bytes"
+#   https://github.com/zarr-developers/zarr-python/issues/487
+STORAGE_CHUNK_SIZE="32"
 
 LAYER_INDICES_LIST="[-1]"
 # LAYER_INDICES_LIST="[-1],[-2]"
 
 # DATA_NUMBER_OF_SAMPLES="128"
-DATA_NUMBER_OF_SAMPLES="3000"
+DATA_NUMBER_OF_SAMPLES="512"
+# DATA_NUMBER_OF_SAMPLES="3000"
 
 # EMBEDDINGS_DATA_PREP_NUM_SAMPLES="1000"
 EMBEDDINGS_DATA_PREP_NUM_SAMPLES="30000"
@@ -38,21 +56,29 @@ ADDITIONAL_OVERRIDES=""
 
 # ==================================================== #
 
-echo "Calling python script ABSOLUTE_PYTHON_SCRIPT_PATH=${ABSOLUTE_PYTHON_SCRIPT_PATH} ..."
+echo ">>> Calling python script ABSOLUTE_PYTHON_SCRIPT_PATH=${ABSOLUTE_PYTHON_SCRIPT_PATH} ..."
 
-poetry run python3 $ABSOLUTE_PYTHON_SCRIPT_PATH \
+uv run python3 $ABSOLUTE_PYTHON_SCRIPT_PATH \
     --multirun \
     data=$DATA_LIST \
-    +data.dataset_type=huggingface_dataset_named_entity \
+    $DATASET_TYPE_LINE \
     language_model=$LANGUAGE_MODEL_LIST \
     +language_model.checkpoint_no=$CHECKPOINT_NO \
+    embeddings.batch_size=$EMBEDDINGS_BATCH_SIZE \
     embeddings.embedding_extraction.layer_indices=$LAYER_INDICES_LIST \
-    data.number_of_samples=$DATA_NUMBER_OF_SAMPLES \
+    "data.data_subsampling.number_of_samples=$DATA_NUMBER_OF_SAMPLES" \
     embeddings_data_prep.sampling.num_samples=$EMBEDDINGS_DATA_PREP_NUM_SAMPLES \
+    "embeddings_data_prep.sampling.sampling_mode=random" \
+    "local_estimates=twonn" \
+    "local_estimates.filtering.num_samples=500" \
+    "local_estimates.compute_global_estimates=True" \
+    "local_estimates.pointwise.n_neighbors_mode=absolute_size" \
+    "local_estimates.pointwise.absolute_n_neighbors=128" \
+    "storage.chunk_size=$STORAGE_CHUNK_SIZE" \
     $ADDITIONAL_OVERRIDES
 
 
-echo "Finished python script."
+echo ">>> Finished python script."
 
 # ==================================================== #
 
