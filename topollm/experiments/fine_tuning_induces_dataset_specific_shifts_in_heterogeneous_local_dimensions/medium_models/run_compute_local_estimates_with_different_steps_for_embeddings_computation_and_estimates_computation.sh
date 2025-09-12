8239546af1ca1ed975bee1e2eeac5414fe3076b0
Benjamin Ruppik <benjamin.ruppik@hhu.de>
2025-09-12 18:13:55 +0200

    Update logging of converted LUSTER token masks to create tables with results per column

--- topollm/experiments/fine_tuning_induces_dataset_specific_shifts_in_heterogeneous_local_dimensions/medium_models/run_compute_local_estimates_with_different_steps_for_embeddings_computation_and_estimates_computation.sh
+++ topollm/experiments/fine_tuning_induces_dataset_specific_shifts_in_heterogeneous_local_dimensions/medium_models/run_compute_local_estimates_with_different_steps_for_embeddings_computation_and_estimates_computation.sh
@@ -199,12 +199,13 @@ LANGUAGE_MODEL_ARGS=(
     # "language_model=Phi-3.5-mini-instruct"
     #
     # "language_model=luster-full"
-    # LUSTER variants without luster-full:
+    # > LUSTER variants without luster-full:
     "language_model=luster-base,luster-base-emotion,luster-chitchat,luster-rl-sent,luster-rl-succ"
     #
     "embeddings_data_prep.token_masking.token_masking_mode=based_on_meta_column"
-    "embeddings_data_prep.token_masking.token_mask_meta_column_name=mask_system_last"
+    # "embeddings_data_prep.token_masking.token_mask_meta_column_name=mask_system_last"
     # "embeddings_data_prep.token_masking.token_mask_meta_column_name=mask_state,mask_database"
+    "embeddings_data_prep.token_masking.token_mask_meta_column_name=mask_system_last,mask_state,mask_database"
     #
     # > All six variants of LUSTER models:
     # "language_model=luster-base,luster-base-emotion,luster-chitchat,luster-full,luster-rl-sent,luster-rl-succ"
